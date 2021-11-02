8fe18f815c11e63949dbf5901528144eadf6cf4d
Tommy Hamarsnes <tommyhamarsnes@gmail.com>
2021-11-02 21:39:43 +0100

    Styling: pink added

--- src/pages/Mvp.tsx
+++ src/pages/Mvp.tsx
@@ -6,7 +6,7 @@ const Mvp = () => {
             <div className="mvp-container z2">
                 <section className="mvp-info-container z2">
                     <div className="mvp-white-container z2">
-                        <p className="mvp-info z2">
+                        <p className="mvp-info z2 pink">
                             Er dere et community som holder gratis meetups? Teknologihuset har flyttet til REBEL i Universitetsgata 2, på St. Olavs plass.
 
                             Ønsker dere å holde en meetup hos oss?
