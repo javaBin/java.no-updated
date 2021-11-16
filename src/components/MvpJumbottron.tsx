97f74ffa7606d0e8d1ed00d838b8b86e3c4a9864
Tommy Hamarsnes <tommyhamarsnes@gmail.com>
2021-11-16 19:09:20 +0100

    Refactored images placement to work. Deployment still not working

--- src/components/MvpJumbottron.tsx
+++ src/components/MvpJumbottron.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
+import background from '../atrium.png';
 
 const MvpJumbottron = () => {
-  return (
-    <>
-      <div className="mvp-container" style={{backgroundImage: `url("images/atrium.jpeg")`}}>
-        <div className="pink mvp-info-box">
-          <h2>
-            Er dere et community som holder gratis meetups? 
+    return (
+        <>
+            <div className="mvp-container" style={{ backgroundImage: `url(${background})` }}>
+                <div className="pink mvp-info-box">
+                    <h2>
+                        Er dere et community som holder gratis meetups?
           </h2>
-          <div className="mvp-info">
-            <p>
-              Teknologihuset har flyttet til REBEL i Universitetsgata 2, på St. Olavs plass.
+                    <div className="mvp-info">
+                        <p>
+                            Teknologihuset har flyttet til REBEL i Universitetsgata 2, på St. Olavs plass.
             </p>
-            <p>
-              Ønsker dere å holde en meetup hos oss?
+                        <p>
+                            Ønsker dere å holde en meetup hos oss?
               Send epost til <a className="no-link" href="mailto:vert@teknologihuset.no?subject=Book meetup">vert@teknologihuset.no</a> eller trykk på knappen under
-            </p> 
-          </div>
-        </div>
-          <a className="no-link" href="mailto:vert@teknologihuset.no?subject=Book meetup">
-            <button className="mvp-book transition-smooth" aria-label="Book meetup">
-              Book
+            </p>
+                    </div>
+                </div>
+                <a className="no-link" href="mailto:vert@teknologihuset.no?subject=Book meetup">
+                    <button className="mvp-book transition-smooth" aria-label="Book meetup">
+                        Book
             </button>
-          </a>
-      </div>
-    </>
-  );
+                </a>
+            </div>
+        </>
+    );
 }
 
 export default MvpJumbottron;
--- src/pages/Mvp.tsx
+++ src/pages/Mvp.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import MvpJumbottron from '../components/MvpJumbottron';
 import './mvp.css'
+import duke_wip from "../duke-wip.png";
 
 const Mvp = () => {
-  return (
-    <article className="jumbo-box max-width max-height">
-      <MvpJumbottron />
-      <img src="images/duke-wip.png" alt="Duke holding sign that says Work in Progress" className="mvp-duke-wip"/>
-    </article>
-  );
+    return (
+        <article className="jumbo-box max-width max-height">
+            <MvpJumbottron />
+            <img src={duke_wip} alt="Duke holding sign that says Work in Progress" className="mvp-duke-wip" />
+        </article>
+    );
 }
 
 export default Mvp;
